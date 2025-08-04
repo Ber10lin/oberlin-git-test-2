93bddf3bea9306eb4b4d116b9f9761e98b53fff7
Oberlin Simanjuntak <oberlinjuntak10@gmail.com>
2025-08-04 23:20:58 +0700

    menambahkan list di file index.html

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 <body>
   <h1>Hello World</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, illo.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
   
 </body>
 </html>
